1a44ad551e3a7e13576c4adcfed8b2a4693063d0
Bruno Lima <bfl3@cin.ufpe.br>
2023-04-25 22:40:34 -0300

    corrections

--- insertion.js
+++ insertion.js
@@ -22,80 +22,80 @@ db.artistas.insertMany([
     pais: "Brasil",
     ano_nasc: 1763,
     ano_morte: 1836,
-    id_artista: "art_00",
+    id_artista: "art_00"
   },
   {
     nome: "Jeremy Strong",
     pais: "EUA",
     ano_nasc: 1865,
     ano_morte: 1898,
-    id_artista: "art_01",
+    id_artista: "art_01"
   },
   {
     nome: "Naomi Jones",
     pais: "Australia",
     ano_nasc: 1979,
-    id_artista: "art_02",
+    id_artista: "art_02"
   },
   {
     nome: "Noel Gallagher",
     pais: "Inglaterra",
     ano_nasc: 1967,
-    id_artista: "art_03",
+    id_artista: "art_03"
   },
   {
     nome: "Anjela Martinez",
     pais: "Colombia",
     ano_nasc: 1984,
-    id_artista: "art_04",
+    id_artista: "art_04"
   },
   {
     nome: "Kendall Johnson",
     pais: "Africa do Sul",
     ano_nasc: 1861,
     ano_morte: 1925,
-    id_artista: "art_05",
+    id_artista: "art_05"
   },
   {
     nome: "Martin O' Brian",
     pais: "Irlanda",
     ano_nasc: 1625,
     ano_morte: 1665,
-    id_artista: "art_06",
+    id_artista: "art_06"
   },
   {
     nome: "Gustavo Valenca",
     pais: "Brasil",
     ano_nasc: 1964,
-    id_artista: "art_07",
+    id_artista: "art_07"
   },
   {
     nome: "Tsang Ing Mings",
     pais: "China",
     ano_nasc: 1959,
     ano_morte: 1981,
-    id_artista: "art_08",
+    id_artista: "art_08"
   },
   {
     nome: "Gilberto Bauhaus",
     pais: "Brasil",
     ano_nasc: 1939,
-    id_artista: "art_09",
+    id_artista: "art_09"
   },
   {
     nome: "Barry Block",
     pais: "Holanda",
     ano_nasc: 1858,
     ano_morte: 1917,
-    id_artista: "art_10",
+    id_artista: "art_10"
   },
   {
     nome: "Gendo Ikari",
     pais: "Japao",
     ano_nasc: 1891,
     ano_morte: 1949,
-    id_artista: "art_11",
-  },
+    id_artista: "art_11"
+  }
 ]);
 
 db.createCollection("obras"); //criar a coleção de obras de arte
@@ -118,71 +118,71 @@ db.obras.insertMany([
   {
     nome: "A Lata e Meta",
     descricao: `Uma instalação de arte que aborda a relação entre a simplicidade
-        e a complexidade, ou entre o que é tangível e o que é intangível.`,
+e a complexidade, ou entre o que é tangível e o que é intangível.`,
     tipo: "instalacao",
     id_obra: "ins_00",
-    artistas_id: db.artistas.findOne({ id_artista: "art_09" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_09" })._id
   },
   {
     nome: "O Guitarrista e o Irmao",
     descricao: `As cores usadas na pintura são mais escuras, representando a tensão
-        e o conflito entre as personagens. A técnica de pintura usada é bastande abstrata,
-        com traços e pinceladas mais agressivas, transmitindo o sentimento de tensão e
-        desconforto na cena.`,
+e o conflito entre as personagens. A técnica de pintura usada é bastande abstrata,
+com traços e pinceladas mais agressivas, transmitindo o sentimento de tensão e
+desconforto na cena.`,
     tipo: "pintura",
     ano: 2009,
     preco: 2530.31,
     id_obra: "pin_00",
-    artistas_id: db.artistas.findOne({ id_artista: "art_03" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_03" })._id
   },
   {
     nome: "A Latina",
     descricao: `A figura feminina se apresenta em uma postura altiva, com a cabeça
-        erguida e os braços cruzados, transmitindo uma sensação de força e determinação.`,
+erguida e os braços cruzados, transmitindo uma sensação de força e determinação.`,
     tipo: "escultura",
     ano: 2019,
     preco: 3337.28,
     id_obra: "esc_00",
-    artistas_id: db.artistas.findOne({ id_artista: "art_04" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_04" })._id
   },
   {
     nome: "O Trevo de Sangue",
     descricao: `A pintura evoca a história turbulenta da Irlanda, com suas guerras
-        e conflitos.`,
+e conflitos.`,
     tipo: "pintura",
     ano: 1662,
     preco: 1158.14,
     id_obra: "pin_01",
-    artistas_id: db.artistas.findOne({ id_artista: "art_06" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_06" })._id
   },
   {
     nome: "Os Povos",
     descricao: `A pintura retrata as diferentes culturas e etnias que
-        compõem a África do Sul e o continente africano como um todo.`,
+compõem a África do Sul e o continente africano como um todo.`,
     tipo: "pintura",
     ano: 1921,
     preco: 999.37,
     id_obra: "pin_02",
-    artistas_id: db.artistas.findOne({ id_artista: "art_05" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_05" })._id
   },
   {
     nome: "A Solidao",
     descricao: `A instalação é composta por uma variedade de elementos,
-        como objetos, luzes, sons e imagens, que trabalham juntos para evocar
-        a sensação de solidão.`,
+como objetos, luzes, sons e imagens, que trabalham juntos para evocar
+a sensação de solidão.`,
     tipo: "instalacao",
     id_obra: "ins_01",
-    artistas_id: db.artistas.findOne({ id_artista: "art_02" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_02" })._id
   },
   {
     nome: "O Poder",
     descricao: `A figura se apresenta de forma majestosa e imponente,
-        evocando a imagem de um líder poderoso.`,
+evocando a imagem de um líder poderoso.`,
     tipo: "escultura",
     ano: 1890,
     preco: 668.35,
     id_obra: "esc_01",
-    artistas_id: db.artistas.findOne({ id_artista: "art_01" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_01" })._id
   },
   {
     nome: "O Verde Brasileiro",
@@ -191,28 +191,28 @@ db.obras.insertMany([
     ano: 1788,
     preco: 3036.7,
     id_obra: "pin_03",
-    artistas_id: db.artistas.findOne({ id_artista: "art_00" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_00" })._id
   },
   {
     nome: "O Futuro",
     descricao: `A pintura apresenta uma paisagem urbana futurista,
-        com arranha-céus altos e imponentes, veículos voadores, tecnologias
-        avançadas e uma atmosfera de progresso e inovação. `,
+com arranha-céus altos e imponentes, veículos voadores, tecnologias
+avançadas e uma atmosfera de progresso e inovação. `,
     tipo: "pintura",
     ano: 1901,
     preco: 3443.62,
     id_obra: "pin_04",
-    artistas_id: db.artistas.findOne({ id_artista: "art_11" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_11" })._id
   },
   {
     nome: "O Ator",
     descricao: `A obra retrata um ator em pose dramática, com
-        gestos exagerados e uma expressão facial intensa.`,
+gestos exagerados e uma expressão facial intensa.`,
     tipo: "escultura",
     ano: 1894,
     preco: 4728.45,
     id_obra: "esc_02",
-    artistas_id: db.artistas.findOne({ id_artista: "art_10" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_10" })._id
   },
   {
     nome: "O Povo",
@@ -221,7 +221,7 @@ db.obras.insertMany([
     ano: 1980,
     preco: 1319.3,
     id_obra: "pin_05",
-    artistas_id: db.artistas.findOne({ id_artista: "art_08" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_08" })._id
   },
   {
     nome: "O Novo Verde Brasileiro",
@@ -230,46 +230,45 @@ db.obras.insertMany([
     ano: 1998,
     preco: 5014.47,
     id_obra: "pin_06",
-    artistas_id: db.artistas.findOne({ id_artista: "art_07" })._id,
+    artistas_id: db.artistas.findOne({ id_artista: "art_07" })._id
   },
   {
     nome: "Brasil-China",
     descricao: `Uma colaboração de artistas famosos de diferentes
-          origens`,
+origens`,
     tipo: "pintura",
     ano: 1980,
     preco: 2914.43,
     id_obra: "pin_07",
     artistas_id: [
       db.artistas.findOne({ id_artista: "art_08" })._id,
-      db.artistas.findOne({ id_artista: "art_09" })._id,
+      db.artistas.findOne({ id_artista: "art_09" })._id
     ],
   },
   {
     nome: "Duas Mulheres, Dois Mundos",
     descricao: `A obra retrata duas mulheres de diferentes lugares
-          do mundo, enfatizando tanto suas similarides quanto suas 
-          disparidades.`,
+do mundo, enfatizando tanto suas similarides quanto suas disparidades.`,
     tipo: "escultura",
     ano: 2014,
     preco: 2128.45,
     id_obra: "esc_03",
     artistas_id: [
       db.artistas.findOne({ id_artista: "art_02" })._id,
-      db.artistas.findOne({ id_artista: "art_04" })._id,
-    ],
+      db.artistas.findOne({ id_artista: "art_04" })._id
+    ]
   },
   {
     nome: "Duas Mulheres, Dois Mundos: O Ato ",
     descricao: `Uma instalação baseada na escultura das artistas de
-          mesmo nome.`,
+mesmo nome.`,
     tipo: "instalacao",
     id_obra: "ins_02",
     artistas_id: [
       db.artistas.findOne({ id_artista: "art_02" })._id,
-      db.artistas.findOne({ id_artista: "art_04" })._id,
-    ],
-  },
+      db.artistas.findOne({ id_artista: "art_04" })._id
+    ]
+  }
 ]);
 
 db.createCollection("galerias"); //criar a coleção de galerias
@@ -295,14 +294,14 @@ db.galerias.insertMany([
       pais: "Brasil",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "ins_02" }),
-      db.obras.findOne({ id_obra: "esc_00" }),
-      db.obras.findOne({ id_obra: "esc_03" }),
-      db.obras.findOne({ id_obra: "ins_00" }),
-      db.obras.findOne({ id_obra: "pin_07" }),
-      db.obras.findOne({ id_obra: "pin_03" }),
-      db.obras.findOne({ id_obra: "pin_06" }),
-    ],
+      db.obras.findOne({ id_obra: "ins_02" })._id,
+      db.obras.findOne({ id_obra: "esc_00" })._id,
+      db.obras.findOne({ id_obra: "esc_03" })._id,
+      db.obras.findOne({ id_obra: "ins_00" })._id,
+      db.obras.findOne({ id_obra: "pin_07" })._id,
+      db.obras.findOne({ id_obra: "pin_03" })._id,
+      db.obras.findOne({ id_obra: "pin_06" })._id
+    ]
   },
   {
     nome: "Galeria Chico Science",
@@ -311,11 +310,11 @@ db.galerias.insertMany([
       pais: "Brasil",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "ins_00" }),
-      db.obras.findOne({ id_obra: "pin_07" }),
-      db.obras.findOne({ id_obra: "pin_03" }),
-      db.obras.findOne({ id_obra: "pin_06" }),
-    ],
+      db.obras.findOne({ id_obra: "ins_00" })._id,
+      db.obras.findOne({ id_obra: "pin_07" })._id,
+      db.obras.findOne({ id_obra: "pin_03" })._id,
+      db.obras.findOne({ id_obra: "pin_06" })._id
+    ]
   },
   {
     nome: "Galeria Mark Twain",
@@ -324,11 +323,11 @@ db.galerias.insertMany([
       pais: "EUA",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "esc_01" }),
-      db.obras.findOne({ id_obra: "esc_02" }),
-      db.obras.findOne({ id_obra: "esc_03" }),
-      db.obras.findOne({ id_obra: "ins_02" }),
-    ],
+      db.obras.findOne({ id_obra: "esc_01" })._id,
+      db.obras.findOne({ id_obra: "esc_02" })._id,
+      db.obras.findOne({ id_obra: "esc_03" })._id,
+      db.obras.findOne({ id_obra: "ins_02" })._id
+    ]
   },
   {
     nome: "Galeria Vermelha",
@@ -337,11 +336,11 @@ db.galerias.insertMany([
       pais: "China",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "pin_05" }),
-      db.obras.findOne({ id_obra: "pin_04" }),
-      db.obras.findOne({ id_obra: "pin_07" }),
-      db.obras.findOne({ id_obra: "esc_01" }),
-    ],
+      db.obras.findOne({ id_obra: "pin_05" })._id,
+      db.obras.findOne({ id_obra: "pin_04" })._id,
+      db.obras.findOne({ id_obra: "pin_07" })._id,
+      db.obras.findOne({ id_obra: "esc_01" })._id,
+    ]
   },
   {
     nome: "Galeria Keane",
@@ -350,12 +349,12 @@ db.galerias.insertMany([
       pais: "Irlanda",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "pin_00" }),
-      db.obras.findOne({ id_obra: "pin_01" }),
-      db.obras.findOne({ id_obra: "pin_02" }),
-      db.obras.findOne({ id_obra: "ins_01" }),
-      db.obras.findOne({ id_obra: "pin_04" }),
-    ],
+      db.obras.findOne({ id_obra: "pin_00" })._id,
+      db.obras.findOne({ id_obra: "pin_01" })._id,
+      db.obras.findOne({ id_obra: "pin_02" })._id,
+      db.obras.findOne({ id_obra: "ins_01" })._id,
+      db.obras.findOne({ id_obra: "pin_04" })._id,
+    ]
   },
   {
     nome: "Galeria Real",
@@ -364,11 +363,11 @@ db.galerias.insertMany([
       pais: "Inglaterra",
     },
     obras_id: [
-      db.obras.findOne({ id_obra: "pin_00" }),
-      db.obras.findOne({ id_obra: "esc_00" }),
-      db.obras.findOne({ id_obra: "pin_02" }),
-      db.obras.findOne({ id_obra: "ins_01" }),
-      db.obras.findOne({ id_obra: "esc_01" }),
-    ],
-  },
+      db.obras.findOne({ id_obra: "pin_00" })._id,
+      db.obras.findOne({ id_obra: "esc_00" })._id,
+      db.obras.findOne({ id_obra: "pin_02" })._id,
+      db.obras.findOne({ id_obra: "ins_01" })._id,
+      db.obras.findOne({ id_obra: "esc_01" })._id
+    ]
+  }
 ]);
--- query.js
+++ query.js
@@ -1,103 +1,171 @@
-// AGGREGATE + PROJECT => nome e idade dos artistas
+// 1.USE => acessar a database
+use arts;
+
+// 2.FIND => retornar os artistas que estão vivos, ordenados pela. mostrar apenas o nome e a data de nascimento
+db.artistas.find({ano_morte:null}, {_id:0, nome:1, ano_nasc:1}).sort({ano_nasc: 1});
+
+// 3.SIZE => retornar as galerias que possuem exatamente 4 obras em seu acervo, mostrando apenas o nome da galeria e os nomes das obras.
+
+// 4.AGGREGATE => nome e idade dos artistas
 db.artistas.aggregate([
   {
     $project: {
       nome: 1,
       idade: {
         $subtract: [{ $ifNull: ["$ano_morte", 2023] }, "$ano_nasc"],
-      },
-    },
+      }
+    }
   },
+  {
+    $project: {
+      _id: 0
+    }
+  }
 ]);
 
-// 8- GROUP => total de obras de cada tipo
+// 8.GROUP => total de obras de cada tipo
 db.obras.aggregate([
   {
     $group: {
       _id: "$tipo",
-      quantidade: { $sum: 1 },
-    },
-  },
+      quantidade: { $sum: 1 }
+    }
+  }
 ]);
 
-// 9- SUM => total do preço de obras em cada galeria
+// 9.SUM => total do preço de obras em cada galeria
 // estamos agrupando por id da galeria
 db.galerias.aggregate([
+
   {
-    $unwind: "$obras_id",
+    $lookup: {
+      from: "obras",
+      localField: "obras_id",
+      foreignField: "_id",
+      as: "obras"
+    }
+  },
+  {
+    $unwind: "$obras"
   },
   {
     $group: {
       _id: "$_id",
-      galeria: { $first: "$nome" }, // $first pega o primeiro valor do array - necessário por causa do group
-      total_preco: { $sum: "$obras_id.preco" },
-    },
+      nome: { $first: "$nome" },
+      total: { $sum: "$obra.preco" }
+    }
   },
+  {
+    $project: {
+      _id: 0
+    }
+  }
 ]);
 
-// 10- COUNT => número de obras cujo preço é maior que 2000
+// 10.COUNT => número de obras cujo preço é maior que 2000
 db.obras.count({ preco: { $gt: 2000 } });
 
-// 11- MAX => o preço da óbra mais cara da Galeria Chico Science
+// 11.MAX => o preço da obra mais cara da Galeria Chico Science
+// estamos agrupando por id da galeria
 db.galerias.aggregate([
   {
     $match: { nome: "Galeria Chico Science" },
   },
   {
-    $unwind: "$obras_id",
+    $lookup: {
+      from: "obras",
+      localField: "obras_id",
+      foreignField: "_id",
+      as: "obras"
+    }
+  },
+  {
+    $unwind: "$obras"
   },
   {
     $group: {
       _id: "$_id",
       galeria: { $first: "$nome" },
-      preco_max: { $max: "$obras_id.preco" },
-    },
+      preco_max: { $max: "$obras.preco" }
+    }
   },
+  {
+    $project: {
+      _id: 0
+    }
+  }
 ]);
 
-// 12- AVG => média das idades dos artistas
+// 12.AVG => média das idades dos artistas
 db.artistas.aggregate([
   {
     $group: {
       _id: null,
       media_idade: {
         $avg: { $subtract: [{ $ifNull: ["$ano_morte", 2023] }, "$ano_nasc"] },
-      },
-    },
+      }
+    }
   },
   {
     $project: {
       _id: 0,
-      media_idade: 1,
-    },
-  },
+      media_idade: 1
+    }
+  }
 ]);
 
-// 13- EXISTS => artistas que já faleceram
+// 13.EXISTS => artistas que já faleceram
 db.artistas.find({ ano_morte: { $exists: true } });
 
-// 14- SORT
+// 14.SORT
 // ordena os artistas por ano de nascimento
 db.artistas.find().sort({ ano_nasc: 1 });
 
-// ordena as obras por ano de criação e selciona as que foram criadas antes do século XXI
+// ordena as obras por ano de criação e selciona as que foram criadas antes do século XXI, mostrando apenas o nome, a descricao, o tipo e o ano de criação da obra
 db.obras.aggregate([
-  { $match: { year: { $lte: 2000 } } },
-  { $sort: { year: 1 } },
+  { 
+    $match: {
+       ano: { $lte: 2000 } 
+      } 
+  },
+  { 
+    $sort: { 
+      ano: 1 
+    } 
+  },
+  {
+    $project: {
+      _id: 0,
+      nome: 1,
+      descricao: 1,
+      tipo: 1,
+      ano: 1 
+    }
+  }
 ]);
 
-// 15- LIMIT
+// 15.LIMIT
 // mostrar só duas obras do tipo "instalação"
 db.obras.find({ tipo: "instalacao" }).limit(2);
 
 // mostrar as duas esculturas mais novas
 db.obras.aggregate([
-  { $match: { tipo: "escultura" } },
-  { $sort: { ano: -1 } },
-  { $limit: 2 },
+  {
+    $match: {
+      tipo: "escultura"
+    }
+  },
+  { 
+    $sort: {
+      ano: -1
+    }
+  },
+  { 
+    $limit: 2
+  }
 ]);
 
-// 16 - $WHERE
+// 16.$WHERE
 // Artistas que morreram com menos de 50 anos
 db.artistas.find({
     $where: function () {
@@ -105,7 +173,7 @@ db.artistas.find({
     },
   }).pretty();
 
-// 17 - MAPREDUCE
+// 17.MAPREDUCE
 // Tipo da obra - Preço total das obras desse tipo cadastradas
 db.obras.mapReduce(
   function () {
@@ -119,149 +187,181 @@ db.obras.mapReduce(
   }
 );
 
-// 18 - FUNCTION
-// artistas brasileiros
-db.artistas.find(function () {
-  return this.pais == "Brasil";
+// 18.FUNCTION
+// retornar artistas brasileiros
+db.artistas.find().toArray().filter(function(doc) {
+  return doc.pais === "Brasil";
 });
 
-// 19 - PRETTY
+
+// 19.PRETTY
 // Obra mais cara
 db.obras.find().sort({preco: -1}).limit(1).pretty();
 
-// 20 - ALL
-//galerias que possuem os tres tipos de obras
-db.galerias.find(
+// 20.ALL
+//retornar as galerias que possuem os tres tipos de obras, mostrando o nome da galeria e aa lista com os nomes e os tipos das obras de cada galeria
+
+db.galerias.aggregate([
   {
-    obras_id: {
-      $all: [
-       {"$elemMatch": { tipo: "instalacao"}},
-        {"$elemMatch": { tipo: "escultura"}},
-        {"$elemMatch": { tipo: "pintura"}}
-      ]
+    $lookup: {
+      from: "obras",
+      localField: "obras_id",
+      foreignField: "_id",
+      as: "obras"
+    }
+  },
+  {
+    $match: {
+      obras: {
+        $all: [
+          { $elemMatch: { tipo: "instalacao" } },
+          { $elemMatch: { tipo: "escultura" } },
+          { $elemMatch: { tipo: "pintura" } }
+        ]
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      nome: 1,
+      "obras.nome": 1,
+      "obras.tipo": 1
     }
   }
-).pretty();
+])
 
-// 21 - SET
+
+// 21.SET
 // Atualizar o preço da obra "A Latina" para 3500
-db.obras.updateOne(
-  {nome: "A Latina"},
-  {
-    $set: {
-       preco: 3500 
-    }
-  }
-)
+db.obras.updateOne({nome: "A Latina"},{$set: {preco: 3500}})
 
-// 22 - TEXT
+// 22.TEXT
 // Obras que tenham a palavra mulheres no titulo ou em sua descrição
 db.obras.find({$text: {$search: "mulheres"}})
 
 
-// 23 - SEARCH
+// 23.SEARCH
 // Artistas que tem o nome Heitor
 db.artistas.find({ $text: { $search: "Heitor" } });
 
-// 24 - FILTER
-// Filtrar as obras dos artistas com preço maior que 1000
-db.artistas.aggregate([
+// 24.FILTER
+// Filtrar apenas as obras do tipo pintura de uma galeria, retornando o nome da galeria e uma lista com os nomes e os tipos das obras de cada uma. Além disso, excluir as galerias sem pinturas.
+db.galerias.aggregate([
+  {
+    $lookup: {
+      from: "obras",
+      localField: "obras_id",
+      foreignField: "_id",
+      as: "obras"
+    }
+  },
   {
     $project: {
+      _id: 0,
       nome: 1,
-      obras_filtradas: {
+      pinturas: {
         $filter: {
           input: "$obras",
           as: "obra",
-          cond: { $gt: ["$$obra.preco", 1000] },
-        },
-      },
-    },
+          cond: { $eq: ["$$obra.tipo", "pintura"] }
+        }
+      }
+    }
   },
+  {
+    $project: {
+      nome: 1,
+      "pinturas.nome": 1,
+      "pinturas.tipo": 1
+    }
+  },
+  {
+    $match: {
+      "pinturas": { $not: {$size: 0} }
+    }
+  }
 ]);
 
-// 25 - UPDATE
+// 25.UPDATE
 // atualizar o preço da obra "O Trevo de Sangue"
 db.obras.updateOne({ nome: "O Trevo de Sangue" }, { $set: { preco: 1200 } });
 
-// 27 - RENAMECOLLECTION
+// 27.RENAMECOLLECTION
 // renomear a coleção "galerias" para "galerias_de_arte"
 db.galerias.renameCollection("galerias_de_arte");
 
-// 28 - COND
+// 28.COND
 // calcular a média de preços das obras, separadas por serem caras ou baratas (definindo caras como preço maior que 2000)
-db.obras.aggregate([
+db.obras.aggregate(
   {
     $group: {
       _id: { $cond: [{ $gt: ["$preco", 2000] }, "Caras", "Baratas"] },
-      media_preco: { $avg: "$preco" },
-    },
+      media_preco: { $avg: "$preco" }
+    }
   },
-]);
+);
 
-// 29 - LOOKUP
+// 29.LOOKUP
 // listar todas as obras e seus respectivos artistas
-db.obras.aggregate([
+db.obras.aggregate(
   {
     $lookup: {
       from: "artistas",
       localField: "artistas_id",
       foreignField: "_id",
-      as: "artistas",
-    },
-  },
-]);
+      as: "artistas"
+    }
+  }
+);
 
-// 30 - FINDONE
+// 30.FINDONE
 // encontrar a primeira obra do tipo "escultura"
 db.obras.findOne({ tipo: "escultura" });
 
-// 31 - ADDTOSET
+// 31.ADDTOSET
 // encontrar todos os países dos artistas sem duplicatas
-db.artistas.aggregate([
+db.artistas.aggregate(
   {
     $group: {
       _id: null,
       paises: { $addToSet: "$pais" },
-    },
-  },
-]);
-
-
-// ? Use "better comments" extension for a proper visualization of the checlist ;)
+    }
+  }
+);
 
 /*
 --CHECKLIST--
 
-! 1. USE 
-! 2. FIND 
-! 3. SIZE 
-! 4. AGGREGATE 
-! 5. MATCH 
-! 6. PROJECT 
-! 7. GTE 
-* 8. GROUP 
-* 9. SUM 
-* 10. COUNT
-* 11. MAX 
-* 12. AVG 
-* 13. EXISTS 
-* 14. SORT 
-* 15. LIMIT 
-* 16. $WHERE
-* 17. MAPREDUCE 
-* 18. FUNCTION 
-* 19. PRETTY 
-* 20. ALL 
-* 21. SET 
-* 22. TEXT 
-* 23. SEARCH 
-* 24. FILTER 
-* 25. UPDATE 
-* 26. SAVE 
-* 27. RENAMECOLLECTION 
-* 28. COND 
-* 29. LOOKUP 
-* 30. FINDONE 
-* 31. ADDTOSET 
+[X] 1. USE 
+[X] 2. FIND 
+[ ] 3. SIZE 
+[ ] 4. AGGREGATE 
+[ ] 5. MATCH 
+[ ] 6. PROJECT 
+[ ] 7. GTE 
+[X] 8. GROUP 
+[X] 9. SUM 
+[X] 10. COUNT
+[X] 11. MAX 
+[X] 12. AVG 
+[X] 13. EXISTS 
+[X] 14. SORT 
+[X] 15. LIMIT 
+[X] 16. $WHERE
+[X] 17. MAPREDUCE 
+[X] 18. FUNCTION 
+[X] 19. PRETTY 
+[X] 20. ALL 
+[X] 21. SET 
+[X] 22. TEXT 
+[X] 23. SEARCH 
+[X] 24. FILTER 
+[X] 25. UPDATE 
+[!] 26. SAVE -> 
+[X] 27. RENAMECOLLECTION 
+[X] 28. COND 
+[X] 29. LOOKUP 
+[X] 30. FINDONE 
+[X] 31. ADDTOSET 
 */
